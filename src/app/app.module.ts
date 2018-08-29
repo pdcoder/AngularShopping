38e6b30f35e060a5b212a408d087790703890c6c
Prakash <prakashdas1998.pd@gmail.com>
2018-08-29 12:55:18 +0530

    Refractored

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CheckoutComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
