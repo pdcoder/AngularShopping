1f2aeb3b2d9d18fd5b8e76f6a4670d62fdb354ec
Prakash <prakashdas1998.pd@gmail.com>
2018-09-01 23:23:57 +0530

    Fixed bug

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
 import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,7 +12,9 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
 
-  constructor(categoryService : CategoryService, productService: ProductService) { 
+  constructor(private categoryService : CategoryService, 
+    private router: Router,
+    private productService: ProductService) { 
     this.categories$ = categoryService.getCategories();
   }
 
